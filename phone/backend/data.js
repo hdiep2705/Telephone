3f58937f2465551669d5a8a15dc54996c2154840
hdiep2705 <hdiep2705@gmail.com>
2021-04-06 14:28:31 +0700

    first commit

---
+++ phone/backend/data.js
@@ -0,0 +1,46 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Redmi not 7',
+      category: 'Xiaomi',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: ' Xiaomi',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: '2',
+      name: 'Redmi not 7',
+      category: 'Xiaomi',
+      image: '/images/d1.jpg',
+      price: 50,
+      brand: ' Xiaomi',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Redmi not 7',
+      category: 'Xiaomi',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' Xiaomi',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    }, {
+      _id: '4',
+      name: 'Redmi not 7',
+      category: 'Xiaomi',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' Xiaomi',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    },
+  ]
+}
